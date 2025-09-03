8ac65328896f80bab1c3e8e9e6d547c103434cf7
Einenlum <contact@einenlum.com>
2025-09-03 12:01:49 +0200

    sort stack types alphabetically to have a better overview

--- lib/StackType.php
+++ lib/StackType.php
@@ -6,22 +6,22 @@ namespace Einenlum\PhpStackDetector;
 
 enum StackType: string
 {
-    case LARAVEL = 'laravel';
-    case SYMFONY = 'symfony';
-    case CRAFT_CMS = 'craft-cms';
-    case STATAMIC = 'statamic';
-    case WORDPRESS = 'wordpress';
-    case LEAF_PHP = 'leaf';
-    case SHOPWARE = 'shopware';
-    case OCTOBER_CMS = 'october-cms';
-    case TEMPEST = 'tempest';
     case BOLT_CMS = 'bolt-cms';
-    case DRUPAL = 'drupal';
-    case LUNAR = 'lunar';
-    case TWILL = 'twill';
-    case WINTER_CMS = 'winter-cms';
-    case TYPO3_CMS = 'typo3-cms';
     case CAKE_PHP = 'cakephp';
-    case GRAV_CMS = 'grav-cms';
     case CODEIGNITER = 'codeigniter';
+    case CRAFT_CMS = 'craft-cms';
+    case DRUPAL = 'drupal';
+    case GRAV_CMS = 'grav-cms';
+    case LARAVEL = 'laravel';
+    case LEAF_PHP = 'leaf';
+    case LUNAR = 'lunar';
+    case OCTOBER_CMS = 'october-cms';
+    case SHOPWARE = 'shopware';
+    case STATAMIC = 'statamic';
+    case SYMFONY = 'symfony';
+    case TEMPEST = 'tempest';
+    case TWILL = 'twill';
+    case TYPO3_CMS = 'typo3-cms';
+    case WINTER_CMS = 'winter-cms';
+    case WORDPRESS = 'wordpress';
 }
