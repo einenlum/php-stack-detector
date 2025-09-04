08bed3d42e82beda16fbb5e6f77e8a938e2a711b
Einenlum <contact@einenlum.com>
2025-09-04 16:52:34 +0200

    refactor detector test with php-stack prefix

--- tests/Unit/DetectorTest.php
+++ tests/Unit/DetectorTest.php
@@ -217,7 +217,7 @@ class DetectorTest extends TestCase
     public function it_detects_stack(string $fixtureFolder, ?string $expectedVersion, StackType $expectedType): void
     {
         $fullConfig = $this->sut->getFullConfiguration(
-            sprintf('%s/../fixtures/%s', __DIR__, $fixtureFolder)
+            sprintf('%s/../fixtures/php-stack/%s', __DIR__, $fixtureFolder)
         );
 
         $this->assertNotNull($fullConfig);
@@ -337,61 +337,61 @@ class DetectorTest extends TestCase
     public static function packagesDataProvider(): array
     {
         return [
-            'Symfony 2' => ['php-stack/symfony/2', '2.3', StackType::SYMFONY],
-            'Symfony 3' => ['php-stack/symfony/3', '3.4', StackType::SYMFONY],
-            'Symfony 4' => ['php-stack/symfony/4', '4', StackType::SYMFONY],
-            'Symfony 5' => ['php-stack/symfony/5', '5.0', StackType::SYMFONY],
-            'Symfony 6' => ['php-stack/symfony/6', '6.3', StackType::SYMFONY],
-            'Symfony unknown version' => ['php-stack/symfony/unknown', null, StackType::SYMFONY],
+            'Symfony 2' => ['symfony/2', '2.3', StackType::SYMFONY],
+            'Symfony 3' => ['symfony/3', '3.4', StackType::SYMFONY],
+            'Symfony 4' => ['symfony/4', '4', StackType::SYMFONY],
+            'Symfony 5' => ['symfony/5', '5.0', StackType::SYMFONY],
+            'Symfony 6' => ['symfony/6', '6.3', StackType::SYMFONY],
+            'Symfony unknown version' => ['symfony/unknown', null, StackType::SYMFONY],
 
-            'Laravel *' => ['php-stack/laravel/star', null, StackType::LARAVEL],
-            'Laravel 5' => ['php-stack/laravel/5', '5.2', StackType::LARAVEL],
-            'Laravel 6' => ['php-stack/laravel/6', '6', StackType::LARAVEL],
-            'Laravel 7' => ['php-stack/laravel/7', '7', StackType::LARAVEL],
-            'Laravel 8' => ['php-stack/laravel/8', '8', StackType::LARAVEL],
-            'Laravel 9' => ['php-stack/laravel/9', '9.8.3', StackType::LARAVEL],
-            'Laravel 10' => ['php-stack/laravel/10', '10', StackType::LARAVEL],
-            'Laravel unknown version' => ['php-stack/laravel/unknown', null, StackType::LARAVEL],
+            'Laravel *' => ['laravel/star', null, StackType::LARAVEL],
+            'Laravel 5' => ['laravel/5', '5.2', StackType::LARAVEL],
+            'Laravel 6' => ['laravel/6', '6', StackType::LARAVEL],
+            'Laravel 7' => ['laravel/7', '7', StackType::LARAVEL],
+            'Laravel 8' => ['laravel/8', '8', StackType::LARAVEL],
+            'Laravel 9' => ['laravel/9', '9.8.3', StackType::LARAVEL],
+            'Laravel 10' => ['laravel/10', '10', StackType::LARAVEL],
+            'Laravel unknown version' => ['laravel/unknown', null, StackType::LARAVEL],
 
-            'Bolt CMS 5' => ['php-stack/bolt_cms/5', '5', StackType::BOLT_CMS],
+            'Bolt CMS 5' => ['bolt_cms/5', '5', StackType::BOLT_CMS],
 
-            'Cake PHP 5' => ['php-stack/cakephp/5', '5.1', StackType::CAKE_PHP],
+            'Cake PHP 5' => ['cakephp/5', '5.1', StackType::CAKE_PHP],
 
-            'Codeigniter 4' => ['php-stack/codeigniter/4', '4', StackType::CODEIGNITER],
+            'Codeigniter 4' => ['codeigniter/4', '4', StackType::CODEIGNITER],
 
-            'Drupal 11' => ['php-stack/drupal/11', '11', StackType::DRUPAL],
+            'Drupal 11' => ['drupal/11', '11', StackType::DRUPAL],
 
-            'Grav CMS 2' => ['php-stack/grav_cms/2', '2', StackType::GRAV_CMS],
+            'Grav CMS 2' => ['grav_cms/2', '2', StackType::GRAV_CMS],
 
-            'Leaf 3' => ['php-stack/leaf/3', '3', StackType::LEAF_PHP],
+            'Leaf 3' => ['leaf/3', '3', StackType::LEAF_PHP],
 
-            'Lunar 1' => ['php-stack/lunar/1', '1', StackType::LUNAR],
+            'Lunar 1' => ['lunar/1', '1', StackType::LUNAR],
 
-            'October CMS 4' => ['php-stack/october_cms/4', '4', StackType::OCTOBER_CMS],
+            'October CMS 4' => ['october_cms/4', '4', StackType::OCTOBER_CMS],
 
-            'Shopware 6' => ['php-stack/shopware/6', '6.7.0', StackType::SHOPWARE],
+            'Shopware 6' => ['shopware/6', '6.7.0', StackType::SHOPWARE],
 
-            'Twill 3' => ['php-stack/twill/3', '3', StackType::TWILL],
+            'Twill 3' => ['twill/3', '3', StackType::TWILL],
 
-            'Typo 3 13' => ['php-stack/typo3_cms/13', '13', StackType::TYPO3_CMS],
+            'Typo 3 13' => ['typo3_cms/13', '13', StackType::TYPO3_CMS],
 
-            'Winter CMS 1' => ['php-stack/winter_cms/1', '1.2', StackType::WINTER_CMS],
+            'Winter CMS 1' => ['winter_cms/1', '1.2', StackType::WINTER_CMS],
 
-            'Wordpress 1' => ['php-stack/wordpress/1', '1.0.2', StackType::WORDPRESS],
-            'Wordpress 2' => ['php-stack/wordpress/2', '2.0.11', StackType::WORDPRESS],
-            'Wordpress 3' => ['php-stack/wordpress/3', '3.0.6', StackType::WORDPRESS],
-            'Wordpress 4' => ['php-stack/wordpress/4', '4.0.38', StackType::WORDPRESS],
-            'Wordpress 5' => ['php-stack/wordpress/5', '5.0.20', StackType::WORDPRESS],
-            'Wordpress 6' => ['php-stack/wordpress/6', '6.3.2', StackType::WORDPRESS],
+            'Wordpress 1' => ['wordpress/1', '1.0.2', StackType::WORDPRESS],
+            'Wordpress 2' => ['wordpress/2', '2.0.11', StackType::WORDPRESS],
+            'Wordpress 3' => ['wordpress/3', '3.0.6', StackType::WORDPRESS],
+            'Wordpress 4' => ['wordpress/4', '4.0.38', StackType::WORDPRESS],
+            'Wordpress 5' => ['wordpress/5', '5.0.20', StackType::WORDPRESS],
+            'Wordpress 6' => ['wordpress/6', '6.3.2', StackType::WORDPRESS],
 
-            'Craft CMS 3' => ['php-stack/craft_cms/3', '3.6', StackType::CRAFT_CMS],
-            'Craft CMS 4' => ['php-stack/craft_cms/4', '4.4', StackType::CRAFT_CMS],
-            'Craft CMS unknown version' => ['php-stack/craft_cms/unknown', null, StackType::CRAFT_CMS],
+            'Craft CMS 3' => ['craft_cms/3', '3.6', StackType::CRAFT_CMS],
+            'Craft CMS 4' => ['craft_cms/4', '4.4', StackType::CRAFT_CMS],
+            'Craft CMS unknown version' => ['craft_cms/unknown', null, StackType::CRAFT_CMS],
 
-            'Statamic 4' => ['php-stack/statamic/4', '4', StackType::STATAMIC],
-            'Statamic unknown version' => ['php-stack/statamic/unknown', null, StackType::STATAMIC],
+            'Statamic 4' => ['statamic/4', '4', StackType::STATAMIC],
+            'Statamic unknown version' => ['statamic/unknown', null, StackType::STATAMIC],
 
-            'Composer lock test' => ['php-stack/symfony-in-composer-lock', '6.3.5', StackType::SYMFONY],
+            'Composer lock test' => ['symfony-in-composer-lock', '6.3.5', StackType::SYMFONY],
         ];
     }
 }
